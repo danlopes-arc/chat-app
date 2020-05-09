aa8f4dde8bfa73e62bbffce16eb1a2fae30ad237
danlopes-arc <danlpsarcanjo@gmail.com>
2020-05-09 14:11:02 -0400

    Add GET support for files in /static

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require("fs")
 const app = require("express")()
 const http = require("http").createServer(app)
 
@@ -10,6 +11,16 @@ const port = 8080
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/static/index.html")
+})
+
+app.get("/static/:file*", (req, res) => {
+    const path = req.path
+    const file = __dirname + path
+    if (!fs.existsSync(file)) {
+        res.sendStatus(404)
+        return
+    }
+    res.sendFile(file)
 })
 
 io.on("connection", socket => {
